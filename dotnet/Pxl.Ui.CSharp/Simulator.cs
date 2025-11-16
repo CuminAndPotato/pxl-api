705a48592ca2d1c6efb1d4259331928076e0981d
SchlenkR <SchlenkR@github>
2025-11-16 21:04:03 +0100

    x

--- dotnet/Pxl.Ui.CSharp/Simulator.cs
+++ dotnet/Pxl.Ui.CSharp/Simulator.cs
@@ -2,12 +2,13 @@ namespace Pxl.Ui.CSharp;
 
 public static class Simulator
 {
-    public static void Start(string host, Action myScene)
+    public static Task Start(string host, Action myScene)
     {
         Pxl.Ui.Simulator.startAction(
             CanvasProxy.createWithDefaults(host),
             myScene);
         if (Pxl.ApiEnv.isInInteractiveContext)
             Console.WriteLine("Simulator started. Press any key to exit...");
+        return Task.Delay(Timeout.Infinite);
     }
 }
